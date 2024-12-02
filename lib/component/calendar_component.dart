c3506264e8b27ab5a26ff4df62933b6c8c2bc417
lsjin <lsjin@wkit.co.kr>
2024-12-02 18:41:27 +0900

    [FEATURE] add toggle(temp)

--- lib/component/calendar_component.dart
+++ lib/component/calendar_component.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class CalendarComponent extends StatefulWidget{
@@ -7,22 +6,102 @@ class CalendarComponent extends StatefulWidget{
 }
 
 class _CalendarComponentState extends State<CalendarComponent>{
-  DateTime date = DateTime.now();
+  DateTime dateContext = DateTime.now();
+  bool isToggled = false;
 
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //       color: ColorScheme.dark().onPrimary,
-    //       // color: Colors.blue,
-    // );
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Container(
-          // color: ColorScheme.dark().onPrimary,
-          color: Colors.blue,
-          // child: Expanded(child: ColoredBox(color: ColorScheme.dark().onPrimary),),
-        )
+        Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                dateContext.year.toString(),
+                style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isToggled = !isToggled;
+                      });
+                    },
+                    child: Container(
+                      width: 60,
+                      height: 30,
+                      padding: const EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: isToggled ? Colors.blue : Colors.grey.shade300,
+                      ),
+                      child: AnimatedAlign(
+                        duration: const Duration(milliseconds: 200),
+                        alignment: isToggled ? Alignment.centerRight : Alignment.centerLeft,
+                        child: Container(
+                          width: 24,
+                          height: 24,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                          ),
+                          child: Icon(
+                            isToggled ? Icons.favorite : Icons.favorite_border,
+                            size: 16,
+                            color: isToggled ? Colors.blue : Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isToggled = !isToggled;
+                      });
+                    },
+                    child: Container(
+                      width: 60,
+                      height: 30,
+                      padding: const EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: isToggled ? Colors.blue : Colors.grey.shade300,
+                      ),
+                      child: AnimatedAlign(
+                        duration: const Duration(milliseconds: 200),
+                        alignment: isToggled ? Alignment.centerRight : Alignment.centerLeft,
+                        child: Container(
+                          width: 24,
+                          height: 24,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                          ),
+                          child: Icon(
+                            isToggled ? Icons.favorite : Icons.favorite_border,
+                            size: 16,
+                            color: isToggled ? Colors.blue : Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),                    
+                ],
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 4,
+          child: Text("test")
+        ),
       ],
     );
   }
